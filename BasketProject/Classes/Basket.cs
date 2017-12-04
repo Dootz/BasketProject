5a2866fe14ffd7ada9291cd0ac7e6ad52fd42dfd
Dootz <k.kozubek@wp.pl>
2017-12-04 19:51:05 +0100

    Initial commit

---
+++ BasketProject/Classes/Basket.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasketProject.Classes
+{
+    class Basket
+    {
+        public List<Item> ItemList { get; private set; }
+
+        /** Add item to basket
+         * @param Item - item to add
+         */
+        public void addItem(Item item)
+        {
+            ItemList.Add(item);
+        }
+
+        /*
+         * Remove all items from list
+         */
+        public void removeAllItems()
+        {
+            ItemList.RemoveAll(IsItem);
+        }
+
+        /**
+         * Check if object is Item
+         * @param object to check
+         */
+        public static bool IsItem(object obj)
+        {
+            if (obj is Item)
+                return true;
+
+            return false;
+        }
+    }
+}
---
+++ BasketProject/Classes/Item.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasketProject.Classes
+{
+    class Item
+    {
+        public String Name { get; private set; }
+        public int Price { get; private set; }
+
+        public Item(String name, int price)
+        {
+            Name = name;
+            Price = price;
+        }
+    }
+}
